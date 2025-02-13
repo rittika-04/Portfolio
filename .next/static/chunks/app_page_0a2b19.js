938b3a32d941f72285f50dd5c0de17df016beed6
RITTIKA SHAW <shaw.rittika15@gmail.com>
2025-02-13 23:12:47 +0530

    Add files via upload

---
+++ .next/static/chunks/app_page_0a2b19.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_0a2b19.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_0a2b19.js",
+  "chunks": [
+    "static/chunks/_f8284b._.js",
+    "static/chunks/node_modules_26911e._.js"
+  ],
+  "source": "dynamic"
+});
